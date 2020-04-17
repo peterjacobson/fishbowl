3b94d596336f6e30148721675e29f1bd990ca10a
Rich Churcher <rich.churcher@gmail.com>
2020-04-18 09:33:11 +1200

    Add @reach/router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Router } from "@reach/router";
 import styled from "styled-components";
 import { ModalProvider } from "styled-react-modal";
 
@@ -7,24 +8,11 @@ import "./App.css";
 import CreateRoom from "./scenes/CreateRoom";
 import JoinRoom from "./scenes/JoinRoom";
 import InsideRoom from "./scenes/InsideRoom";
-import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import heartworklogo from "./img/heartworklogo.png";
+// import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
 
 import useQueryString from "./hooks/useQueryString";
 
-const HeartworkLink = styled.a`
-  position: absolute;
-  background: url(${heartworklogo});
-  background-size: cover;
-  width: 60px;
-  height: 60px;
-  margin-top: -26px;
-  margin-left: -29px;
-  z-index: 200;
-`;
-
 const AppWrapper = styled.div`
-  /* justify-self: center; */
   max-width: 600px;
   margin-left: auto;
   margin-right: auto;
@@ -35,11 +23,10 @@ function App() {
   const [userId, setUserId] = useState();
   const [room, setroom] = useState();
   const [error, setError] = useState();
-
   // Use a custom hook to subscribe to the grocery list ID provided as a URL query parameter
   const [roomId, setroomId] = useQueryString("listId");
 
-  // Use an effect to authenticate and load the grocery list from the database
+  // For Create Room & Join Room
   useEffect(() => {
     FirestoreService.authenticateAnonymously()
       .then((userCredential) => {
@@ -66,12 +53,7 @@ function App() {
     setUser(userName);
   }
 
-  function onCloseroom() {
-    setroomId();
-    setroom();
-    setUser();
-  }
-
+  // Just for Join Room
   function onSelectUser(userName) {
     setUser(userName);
     FirestoreService.getroom(roomId)
@@ -79,43 +61,34 @@ function App() {
       .catch(() => setError("grocery-list-get-fail"));
   }
 
-  // render a scene based on the current state
-
-  function renderScene() {
-    if (room && user) {
-      return (
-        <InsideRoom
-          users={room.users}
-          {...{ roomId, user, onCloseroom, userId }}
-        ></InsideRoom>
-      );
-    } else if (room) {
-      return (
-        <>
-          <ErrorMessage errorCode={error}></ErrorMessage>
-          <JoinRoom
-            users={room.users}
-            {...{ roomId, onSelectUser, onCloseroom, userId }}
-          ></JoinRoom>
-        </>
-      );
-    }
-    return (
-      <>
-        <ErrorMessage errorCode={error}></ErrorMessage>
-        <CreateRoom onCreate={onroomCreate} userId={userId}></CreateRoom>
-      </>
-    );
-  }
-
   return (
     <AppWrapper>
       <ModalProvider>
-        <HeartworkLink
-          href="http://heartwork.co.nz/"
-          target="_blank"
-        ></HeartworkLink>
-        {renderScene()}
+        <Router>
+          <CreateRoom path="/" onCreate={onroomCreate} userId={"sdlfkjlksdjf"}>
+            {/* Welcome */}
+            {/* Room Config */}
+            {/* Share link to room */}
+          </CreateRoom>
+          <JoinRoom
+            path="/join/:roomId"
+            users={[]}
+            roomId={"ljdslkjfds"}
+            userId={"dsljkdsf"}
+            // {...{ roomId, onSelectUser, userId }}
+          />
+          <InsideRoom
+            path="/room/:roomId/user/:userId"
+            users={[]}
+            roomId={"ljdslkjfds"}
+            userId={"dsljkdsf"}
+          >
+            <p>INSIDE</p>
+            {/* Select Checkin */}
+            {/* ? Spoken Checkin  */}
+            {/* Next steps */}
+          </InsideRoom>
+        </Router>
       </ModalProvider>
     </AppWrapper>
   );
