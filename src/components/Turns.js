a1a2efa5a2b18f8ae6a3661ff23a21df5f818ea8
Peter Jacobson <peterjacobson@Peters-MacBook-Pro.local>
2020-05-02 23:28:09 +1200

    better turn messaging

--- src/components/Turns.js
+++ src/components/Turns.js
@@ -1,11 +1,62 @@
 import React from "react";
 import _ from "lodash";
 import styled from "styled-components";
+
 import { rounds } from "../data/roundData";
+import { ButtonWithText } from "./styledComponents";
 
 export function Turns({ room }) {
-  const { teamNames, currentTeam, team0, team1, currentPlayers, round } = room;
+  const userId =
+    window.location.pathname.match(/(?<=(user\/))(.*?)(?=(\/bowl))/g)[0] || "";
+  const {
+    teamNames,
+    currentTeam,
+    team0,
+    team1,
+    currentPlayers,
+    round,
+    turnActive,
+  } = room;
   const teams = [team0, team1];
+  const turnPlayer = _.get(
+    teams,
+    [currentTeam, currentPlayers[currentTeam]],
+    {}
+  );
+  const iAmTurnPlayer = turnPlayer.userId === userId;
+  const iAmTurnTeam = _.find(
+    teams[currentTeam],
+    (player) => player.userId === userId
+  );
+
+  const preTurnActiveTeamMessage = (
+    <>
+      <b>GET READY TO GUESS</b> {turnPlayer.name}'s words/phrases
+      <br />
+      <br />
+      <b>Pin {turnPlayer.name}'s video so you don't lose them!</b>
+    </>
+  );
+
+  const preTurnInactiveTeamMessage =
+    "Relax and prepare to enjoy some silliness";
+
+  const preTurn = iAmTurnPlayer ? (
+    <ButtonWithText>START MY TURN</ButtonWithText>
+  ) : (
+    <>
+      Waiting for {turnPlayer.name} to start their turn
+      <br />
+      <br />
+      {iAmTurnPlayer
+        ? null
+        : iAmTurnTeam
+        ? preTurnActiveTeamMessage
+        : preTurnInactiveTeamMessage}
+    </>
+  );
+
+  const duringTurn = "DURINGTURN";
 
   return (
     <>
@@ -13,11 +64,13 @@ export function Turns({ room }) {
       Turn: {teamNames[currentTeam]}
       <br />
       <br />
-      Player:{" "}
-      {_.get(teams, [currentTeam, currentPlayers[currentTeam]], {}).name}
+      Player: {turnPlayer.name}
       <br />
       <br />
       {rounds[round].rules}
+      <br />
+      <br />
+      {turnActive ? duringTurn : preTurn}
     </>
   );
 }
