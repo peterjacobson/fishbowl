806216fce6535f828e3bc4e56fd1a1919a8aa7a1
Peter Jacobson <peterjacobson@Peters-MacBook-Pro.local>
2020-05-03 02:01:53 +1200

    add timer styled well but not working yet

--- src/components/EasyTimer.js
+++ src/components/EasyTimer.js
@@ -1,46 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { Line } from "rc-progress";
-import moment from "moment";
-import styled from "styled-components";
-
-const ConfigContainer = styled.div`
-  background-color: white;
-  padding: 10px;
-  border-radius: 20px;
-  -webkit-box-shadow: 3px 4px 5px 0px rgba(0, 0, 0, 0.75);
-  -moz-box-shadow: 3px 4px 5px 0px rgba(0, 0, 0, 0.75);
-  box-shadow: 3px 4px 5px 0px rgba(0, 0, 0, 0.75);
-`;
-
-const LinkClick = styled.span`
-  background: none;
-  color: black;
-  text-decoration: underline;
-  box-shadow: none;
-`;
 
 export default function EasyTimer(props) {
-  const [timeRemaining, setTimeRemaining] = useState(undefined);
+  const [timeRemaining, setTimeRemaining] = useState(60);
   const timerLength = props.timerLength * 1000; // ms
-  const timerLengthHR =
-    props.timerLength < 60
-      ? `00:${props.timerLength}s pp`
-      : moment.duration(props.timerLength, "seconds").format("m:ss") +
-        " mins pp";
-  const timeRemainingS = Math.round(timeRemaining / 1000);
-  const timeRemainingHR =
-    timeRemainingS < 60
-      ? `00:${timeRemainingS}s`
-      : moment.duration(timeRemainingS, "seconds").format("m:ss");
 
   function ticker() {
-    // const timeRemaining =
-    //   timerLength - timeDelayFromTimerStartedToStartTimeRecieved;
     return setInterval(() => {
       const timeDelayFromTimerStartedToStartTimeRecieved =
-        Date.now() - props.timer.startTime; // ms
-      const timeRemaining =
-        timerLength - timeDelayFromTimerStartedToStartTimeRecieved;
+        Date.now() - props.startTime; // ms
+      const timeRemaining = Math.floor(
+        (timerLength - timeDelayFromTimerStartedToStartTimeRecieved) / 1000
+      );
+      if (timeRemaining < 0) return props.endTurn();
       setTimeRemaining(timeRemaining);
     }, 1000);
   }
@@ -50,24 +21,9 @@ export default function EasyTimer(props) {
     return () => {
       clearInterval(activeTicker);
     };
-  }, [props.timer]);
+  }, []);
 
-  return timeRemaining == undefined ? null : timeRemaining > 0 &&
-    props.timer.userName ? (
-    <ConfigContainer>
-      {/* <Line
-        percent={(1 - timeRemaining / timerLength) * 100}
-        strokeColor={["#88C072", "#85D262", "#FE8C6A"]}
-        trailColor={["#eef"]}
-      /> */}
-      <h1>{`${props.timer.userName} is checking in for another ${timeRemainingHR}`}</h1>
-      <LinkClick onClick={props.stopTimerNow}>end timer now</LinkClick>
-    </ConfigContainer>
-  ) : (
-    <ConfigContainer>
-      <button onClick={props.startTimerNow}>
-        Start my {timerLength / 1000} second checkin
-      </button>
-    </ConfigContainer>
-  );
+  return timeRemaining === undefined ? null : timeRemaining > 0 ? (
+    <h2>{timeRemaining}</h2>
+  ) : null;
 }
--- src/components/Turn.js
+++ src/components/Turn.js
@@ -2,7 +2,8 @@ import React from "react";
 import _ from "lodash";
 
 import * as FirestoreService from "../services/firestore";
-import { ButtonWithText } from "./styledComponents";
+import { ButtonWithText, VertSpacer } from "./styledComponents";
+import EasyTimer from "./EasyTimer";
 
 export default function Turn({ room, iAmTurnPlayer, iAmTurnTeam }) {
   const userId =
@@ -19,6 +20,7 @@ export default function Turn({ room, iAmTurnPlayer, iAmTurnTeam }) {
     currentTeam,
     points,
     currentPlayers,
+    startTime,
   } = room;
   const teams = [team0, team1];
 
@@ -55,6 +57,24 @@ export default function Turn({ room, iAmTurnPlayer, iAmTurnTeam }) {
     }
   }
 
+  function endTurn() {
+    //update turnActive
+    //update currentPlayers
+    //update currentTeam
+    //shuffle wordsleft
+    const nextRoundWordPhrasesLeft = _.shuffle(roundWordPhrasesLeft.slice(1));
+    const nextCurrentPlayers = currentPlayers.map((player, i) =>
+      i === currentTeam ? (player + 1) % teams[i].length : player
+    );
+    const nextCurrentTeam = currentTeam === 0 ? 1 : 0;
+    FirestoreService.endTurn(
+      roomId,
+      nextCurrentPlayers,
+      nextCurrentTeam,
+      nextRoundWordPhrasesLeft
+    );
+  }
+
   const myTurn = (
     <>
       Express this to your team:
@@ -62,19 +82,27 @@ export default function Turn({ room, iAmTurnPlayer, iAmTurnTeam }) {
       <h3>
         <b>{_.get(roundWordPhrasesLeft, 0, "loading...")}</b>
       </h3>
+      <VertSpacer />
       <ButtonWithText onClick={teamGotIt}>DONE - NEXT!</ButtonWithText>
+      <EasyTimer startTime={startTime} endTurn={endTurn} />
     </>
   );
 
   const myTeamsTurn = (
-    <h3>
-      <b>Guess the Word/Phrase!!!</b>
-    </h3>
+    <>
+      <h3>
+        <b>Guess the Word/Phrase!!!</b>
+      </h3>
+      <EasyTimer startTime={startTime} endTurn={() => console.log("dummy")} />
+    </>
   );
   const otherTeamsTurn = (
-    <p>
-      <b>Relax</b>, just keep the other team honest ;)
-    </p>
+    <>
+      <p>
+        <b>Relax</b>, just keep the other team honest ;)
+      </p>
+      <EasyTimer startTime={startTime} endTurn={() => console.log("dummy")} />
+    </>
   );
 
   if (iAmTurnPlayer) {
